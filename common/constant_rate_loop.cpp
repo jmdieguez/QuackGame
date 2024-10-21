a750feec5a8d871ba327b86126199df5e74847c1
jdieguez1 <jdieguez@fi.uba.ar>
2024-10-21 10:02:09 -0300

    Creo una clase auxiliar para convertir segundos a steady::clock:duration

--- common/constant_rate_loop.cpp
+++ common/constant_rate_loop.cpp
@@ -1,12 +1,20 @@
 #include "constant_rate_loop.h"
 
+using namespace std::chrono;
+
+#define NANOSECONDS_PER_SECOND 1e9
+
+template<typename T>
+steady_clock::duration steady_duration(T seconds) {
+    return steady_clock::duration(static_cast<int64_t>(seconds * NANOSECONDS_PER_SECOND));
+}
+
 ConstantRateLoop::ConstantRateLoop(bool &k_r, const double &r, const std::function<void(int)> &f) :
     keep_running(k_r), rate(r), func(f) {}
 
 ConstantRateLoop::~ConstantRateLoop() {}
 
 void ConstantRateLoop::execute() {
-    using namespace std::chrono;
     steady_clock::time_point t1 = steady_clock::now();  // Initial time point
     int it = 0;
 
@@ -21,13 +29,13 @@ void ConstantRateLoop::execute() {
             // Calculate how many frames are missed and reset t1 to the correct point
             double behind = -rest;  // This is always positive
             double lost = behind - std::fmod(behind, rate);
-            t1 = steady_clock::now();  // Reset to now, correcting for the lost time
+            t1 += steady_duration(lost);
             it += static_cast<int>(lost / rate);  // Skip frames as needed
         } else {
             std::this_thread::sleep_for(duration<double>(rest));  // Wait to maintain rate
-            t1 = steady_clock::now();  // Reset t1 to current time after the sleep
         }
 
+        t1 += steady_duration(rate);
         ++it;  // Increment iteration
     }
 }
