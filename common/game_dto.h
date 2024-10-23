0a07f4b08c5bd799fab079a2382651b60ae54d9e
jdieguez1 <jdieguez@fi.uba.ar>
2024-10-22 22:33:22 -0300

    Añadir DuckDTOs a GameDTO

--- common/game_dto.h
+++ common/game_dto.h
@@ -1,6 +1,7 @@
 #ifndef COMMON_GAME_DTO
 #define COMMON_GAME_DTO
 
+#include "duck_dto.h"
 #include "map_dto.h"
 
 class GameDTO {
@@ -8,7 +9,8 @@ public:
     // acá se guardarian todo lo relacionado al estado actual del juego:
     // mapa, patos, drops, balas, etc
     const MapDTO map_dto;
-    GameDTO(MapDTO &m_dto) : map_dto(m_dto) {}
+    const std::vector<DuckDTO> duck_dtos;
+    GameDTO(MapDTO &m_dto, std::vector<DuckDTO> &vec) : map_dto(m_dto), duck_dtos(vec) {}
     ~GameDTO() {}
 };
 
--- server/game/game.cpp
+++ server/game/game.cpp
@@ -13,5 +13,11 @@ void Game::step() {
 
 GameDTO Game::get_status() {
     MapDTO map_dto = map.get_status();
-    return GameDTO(map_dto);
+    
+    std::vector<DuckDTO> duck_dtos;
+    for (Duck duck : ducks) {
+        duck_dtos.push_back(duck.get_status());
+    }
+
+    return GameDTO(map_dto, duck_dtos);
 }
