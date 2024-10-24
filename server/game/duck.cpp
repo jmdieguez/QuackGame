72e38482ca42547432dd0ac13896db5cdee9d425
MatiasRueda <matiasdruedaa@gmail.com>
2024-10-24 03:48:35 -0300

    Arreglo pequenios bugs

--- server/game/duck.cpp
+++ server/game/duck.cpp
@@ -3,21 +3,23 @@
 #define X_VELOCITY 4
 
 Duck::Duck(const uint8_t &i, const uint16_t &initial_x, const uint16_t &initial_y) : id(i), position(initial_x, initial_y) {}
-    
+
 Duck::~Duck() {}
 
-void Duck::move(Direction &direction) {
+void Duck::move(Direction &direction)
+{
     action = DuckAction::MOVING;
-    switch (direction) {
-        case Direction::RIGHT:
-            status.looking_right = true;
-            break;
-        case Direction::LEFT:
-            status.looking_right = false;
-            break;
-        default:
-            action = DuckAction::IDLE;
-            break;
+    switch (direction)
+    {
+    case Direction::RIGHT:
+        status.looking_right = true;
+        break;
+    case Direction::LEFT:
+        status.looking_right = false;
+        break;
+    default:
+        action = DuckAction::IDLE;
+        break;
     }
 }
 
@@ -29,16 +31,18 @@ void Duck::stop_moving() { action = DuckAction::IDLE; }
 
 void Duck::pickup_gun(std::shared_ptr<Gun> &gun_ptr) { gun = gun_ptr; }
 
-void Duck::drop_gun() { 
+void Duck::drop_gun()
+{
     stop_shooting();
-    gun = nullptr; 
+    gun = nullptr;
 }
 
 void Duck::shoot() { status.shooting = true; }
 
 void Duck::stop_shooting() { status.shooting = false; }
 
-void Duck::flap() { 
+void Duck::flap()
+{
     action = DuckAction::FLAPPING;
     status.shooting = false;
 }
@@ -47,37 +51,50 @@ void Duck::jump() { action = DuckAction::JUMPING; }
 
 void Duck::lay() { action = DuckAction::LAYING; }
 
-void Duck::step() {
-    if (action == DuckAction::MOVING) {
-        if (status.looking_right) {
+void Duck::step()
+{
+    if (action == DuckAction::MOVING)
+    {
+        if (status.looking_right)
+        {
             position.move_x(X_VELOCITY);
-        } else {
+        }
+        else
+        {
             position.move_x(-X_VELOCITY);
         }
-    } 
-    
-    if (status.shooting && gun != nullptr) {
+    }
+
+    if (status.shooting && gun != nullptr)
+    {
         ShootEvent shoot_event = gun->shoot(status.looking_right, status.looking_up);
     }
 }
 
 // true if duck dies after receiving the shot
-bool Duck::receive_shot() { 
-    if (status.has_chestplate) {
+bool Duck::receive_shot()
+{
+    if (status.has_chestplate)
+    {
         status.has_chestplate = false;
         return false;
-    } else if (status.has_helmet) {
+    }
+    else if (status.has_helmet)
+    {
         status.has_helmet = false;
         return false;
-    } else {
+    }
+    else
+    {
         return true;
     }
 }
 
-DuckSnapshot Duck::get_status() {
+DuckSnapshot Duck::get_status()
+{
     PositionSnapshot position_snapshot = position.get_status();
     GunSnapshot gun_snapshot = gun->get_status();
-    return DuckSnapshot(id,
+    return DuckSnapshot(id, 100,
                         position_snapshot,
                         action,
                         status,
