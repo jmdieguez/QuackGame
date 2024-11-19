32720277ff36660b5bfae09e2b0266e6d4aff58e
MatiasRueda <matiasdruedaa@gmail.com>
2024-11-19 12:47:33 -0300

    separo el movimiento vertical

--- server/game/duck/duck.cpp
+++ server/game/duck/duck.cpp
@@ -99,64 +99,15 @@ void Duck::step(Map &map, std::vector<std::shared_ptr<Projectile>> &projectiles,
 {
     if (status.mooving)
         move_horizontal(position, status, map);
-    Position below_left(position.x, position.y + DUCK_HITBOX_Y);
-    Position below_right(position.x + DUCK_HITBOX_X - 1, position.y + DUCK_HITBOX_Y);
-    status.grounded = map.has_something_in(below_left) || map.has_something_in(below_right);
-    status.falling = false;
-    if (status.grounded)
-    {
-        if (status.jumping)
-        {
-            y_velocity = Y_VELOCITY_ON_JUMP; // take a big impulse at the start
-            status.jumping = false;
-            status.falling = false;
-            status.start_jumping = true;
-        }
-        else
-        {
-            y_velocity = Y_VELOCITY_INITIAL;
-            status.start_jumping = false;
-        }
-    }
-    else
-    {
-        if (y_velocity < 0)
-            status.falling = true;
 
-        if (status.flapping && y_velocity < 0)
-        {
-            y_velocity = -2;
-            status.flapping = false;
-            return;
-        }
-        status.flapping = false;
-        y_velocity -= 1;
-    }
+    collision_detector(position, status, map);
+    if (status.grounded)
+        update_jump_status(status, y_velocity);
+    else
+        update_in_the_air_status(status, y_velocity);
 
     if (y_velocity != Y_VELOCITY_INITIAL)
-    {
-        std::function<int(int, int)> operation = (y_velocity < Y_VELOCITY_INITIAL) ? [](int a, int b)
-        { return a + b; }                                                          : // if falling, increment y
-                                                     [](int a, int b)
-        { return a - b; }; // if elevating, decrease y
-        int i = 1;
-        int abs_y_velocity = std::abs(y_velocity);
-        while (i <= abs_y_velocity)
-        {
-            Position new_position(position.x, operation(position.y, 1));
-            Position end_hitbox(new_position.x + DUCK_HITBOX_X - 1, new_position.y + DUCK_HITBOX_Y - 1); // El duck ocupa 18x24
-            if (map.validate_coordinate(new_position) && map.validate_coordinate(end_hitbox))
-            {
-                position = new_position;
-                i++;
-            }
-            else
-            {
-                y_velocity = Y_VELOCITY_INITIAL;
-                break;
-            }
-        }
-    }
+        move_vertical(position, map, y_velocity);
 
     if (status.shooting && !block_shooting_command && gun != nullptr)
         GunController::shoot(status, position, map, projectiles, sounds);
--- server/game/duck/movecontroller.h
+++ server/game/duck/movecontroller.h
@@ -37,5 +37,70 @@ public:
             }
         }
     };
+
+    void collision_detector(Position &position, DuckStatus &status, Map &map)
+    {
+        Position below_left(position.x, position.y + DUCK_HITBOX_Y);
+        Position below_right(position.x + DUCK_HITBOX_X - 1, position.y + DUCK_HITBOX_Y);
+        status.grounded = map.has_something_in(below_left) || map.has_something_in(below_right);
+        status.falling = false;
+    }
+
+    void update_jump_status(DuckStatus &status, int &y_velocity)
+    {
+        if (status.jumping)
+        {
+            y_velocity = Y_VELOCITY_ON_JUMP; // take a big impulse at the start
+            status.jumping = false;
+            status.falling = false;
+            status.start_jumping = true;
+        }
+        else
+        {
+            y_velocity = Y_VELOCITY_INITIAL;
+            status.start_jumping = false;
+        }
+    }
+
+    void update_in_the_air_status(DuckStatus &status, int &y_velocity)
+    {
+        if (y_velocity < 0)
+            status.falling = true;
+
+        if (status.flapping && y_velocity < 0)
+        {
+            y_velocity = -2;
+            status.flapping = false;
+            return;
+        }
+        status.flapping = false;
+        y_velocity -= 1;
+    }
+
+    void move_vertical(Position &position, Map &map, int &y_velocity)
+    {
+        std::function<int(int, int)> operation = (y_velocity < Y_VELOCITY_INITIAL) ? [](int a, int b)
+        { return a + b; }                                                          : // if falling, increment y
+                                                     [](int a, int b)
+        { return a - b; }; // if elevating, decrease y
+        int i = 1;
+        int abs_y_velocity = std::abs(y_velocity);
+        while (i <= abs_y_velocity)
+        {
+            Position new_position(position.x, operation(position.y, 1));
+            Position end_hitbox(new_position.x + DUCK_HITBOX_X - 1, new_position.y + DUCK_HITBOX_Y - 1); // El duck ocupa 18x24
+            if (map.validate_coordinate(new_position) && map.validate_coordinate(end_hitbox))
+            {
+                position = new_position;
+                i++;
+            }
+            else
+            {
+                y_velocity = Y_VELOCITY_INITIAL;
+                break;
+            }
+        }
+    }
+
     ~MoveController() {};
 };
