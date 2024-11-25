a2a5560d31224eb8a4ca7090fb8fd830b7e8d360
jdieguez1 <jdieguez@fi.uba.ar>
2024-11-25 19:33:57 -0300

    Eliminar las armas al inicializar una ronda

--- server/game/game.cpp
+++ server/game/game.cpp
@@ -342,7 +342,8 @@ void Game::remove_gun(const uint16_t &id)
 void Game::step()
 {
     if (initialize)
-    {
+    {   
+        guns.clear();
         for (const auto &position : maps[current_map].get_boxes_spawns())
             boxes.emplace(position, Box::BOX_4_HP);
 
